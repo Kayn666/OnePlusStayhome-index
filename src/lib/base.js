b494e2e619cf6c1a1e5f8e1496bbf83d5e94c449
kayn.li <747893054@qq.com>
2020-11-24 11:27:56 +0800

    name优化

--- src/lib/base.js
+++ src/lib/base.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost/hotel-admin/public/index.php';
+const URL = 'http://localhost/OnePlusStayhome-php/public/index.php';
 const IMGURL = 'http://localhost';
 const SUCCESS = 200;
 const FAIL = 404;
